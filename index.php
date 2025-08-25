16080d46ba30e71cc4f0f4049885d37835e9f793
Tesa Firna Ananta <h1101241057@student.untan.ac.id>
2025-08-25 19:39:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ $waktu = date("Y-m-d H:i:s");
         </div>
 
         <div class="nav">
-            <a href="home.php"> Halaman Lain </a>
+            <a href="Home.php"> Halaman Lain </a>
         </div>
     </div>
 </body>
